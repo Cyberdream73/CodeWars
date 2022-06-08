5400a968e776ea33ac2304a710c3b846ba655d06
cyberdream73 <thomas.vicario73@gmail.com>
2022-06-08 10:49:30 +0200

    name shuffler.js

---
+++ 8th kyu/Name Shuffler.js
@@ -0,0 +1,15 @@
+/* DESCRIPTION:
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+*/
+
+// ANSWER //
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ');
+  }
