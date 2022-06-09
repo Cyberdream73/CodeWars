be520eb301d51e8987e3e06fc42de5ed64f14dd0
cyberdream73 <thomas.vicario73@gmail.com>
2022-06-09 11:23:54 +0200

    reduce but grow.js

---
+++ 8th kyu/Beginner - Reduce but Grow.js
@@ -0,0 +1,11 @@
+/* Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+*/
+
+// ANSWER //
+
+function grow(x){
+    return x.reduce( (acc, current) => acc *= current, 1);
+  }
