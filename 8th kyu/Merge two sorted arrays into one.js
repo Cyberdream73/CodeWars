963cb25056ed887c1e9c781c6e0212b8326161fc
cyberdream73 <thomas.vicario73@gmail.com>
2022-07-11 09:59:46 +0200

    merge two sorted arrays into one.js

---
+++ 8th kyu/Merge two sorted arrays into one.js
@@ -0,0 +1,31 @@
+/* You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+Examples (input -> output)
+* [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+Happy coding!
+
+*/
+
+// ANSWER //
+
+function mergeArrays(arr1, arr2) {
+    let answer = []
+    let arr = arr1 + ',' + arr2
+    arr = arr.split(',')
+    arr.sort( (a, b) => a-b)
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== arr[i+1]) { answer.push(Number(arr[i]))}
+    }
+    if (arr1.length < 1 && arr2.length < 1) { return [];}
+    if (arr1.length < 1) { return arr2.sort((a,b) => a-b);}
+    if (arr2.length < 1) { return arr1.sort((a,b) => a-b);}
+    return answer;
+  }
