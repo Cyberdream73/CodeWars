2d0331b9a6799c128083fd89e5ea3970c1703f6d
cyberdream73 <thomas.vicario73@gmail.com>
2022-07-24 12:36:34 +0200

    find the position.js

---
+++ 8th kyu/Find the position!.js
@@ -0,0 +1,14 @@
+/* When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+*/
+
+// ANSWER //
+
+function position(letter){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`;
+  }
