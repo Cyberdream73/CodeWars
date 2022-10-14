5dbd16a24c4f9d3f3664f67f565fee1b8a8f4fb3
cyberdream73 <thomas.vicario73@gmail.com>
2022-10-14 12:16:23 +0200

    kata example twist.js

---
+++ 8th kyu/Kata Example Twist.js
@@ -0,0 +1,14 @@
+/* This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+*/
+
+// ANSWER //
+
+let websites = []
+let counter = 0
+while (counter < 1000) {
+  websites.push('codewars')
+  counter++
+}
