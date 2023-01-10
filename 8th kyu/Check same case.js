1d88189e2b43417fbf0f539fd5dfede65f61fd3e
cyberdream73 <thomas.vicario73@gmail.com>
2023-01-10 12:43:19 +0100

    check same case.js

---
+++ 8th kyu/Check same case.js
@@ -0,0 +1,32 @@
+/* Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+
+*/
+
+// ANSWER //
+
+function sameCase(a, b){
+    const REGEX = /^[a-zA-Z]+$/
+    if (REGEX.test(a) && REGEX.test(b)) {
+      return caseOfString(a) === caseOfString(b) ? 1 : 0;
+    }else {
+      return -1;
+    }
+  }
+    
+  function caseOfString(str) {
+    return str === str.toLowerCase() ? 'lowercase' : 'uppercase';
+  }
